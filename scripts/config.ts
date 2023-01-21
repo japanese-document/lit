265bc7df6045e3660da5eda437b3df7dd2e0da7e
38elements <mh19820223@gmail.com>
2023-01-21 20:36:16 +0900

    add scripts

---
+++ scripts/config.ts
@@ -0,0 +1,16 @@
+export const TITLE = /__TITLE__/g
+export const BODY = '__BODY__'
+export const INDEX = '__INDEX__'
+export const CSS = '__CSS__'
+export const CSS_PATH = '/tips/app.css?v=001'
+export const BASE_URL = 'https://japanese-document.github.io/tips'
+export const URL = '__URL__'
+export const DESCRIPTION = /__DESCRIPTION__/g
+export const SEPARATOR = /---(.*)/s
+export const PAGE_LAYOUT = 'src/layout.html'
+export const INDEX_PAGE_HEADER = 'もくじ'
+export const INDEX_PAGE_TITLE = 'Tips'
+export const INDEX_PAGE_DESCRIPTION = 'もくじ'
+export const INDEX_PAGE_LAYOUT = 'src/layout.html'
+export const OUTPUT_DIR = './docs'
+export const SOURCE_DIR = './src'
