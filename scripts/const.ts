3f108be45f8b71c098ec7290abc8e92570371971
38elements <mh19820223@gmail.com>
2023-11-17 18:02:46 +0900

    コンテキストを定義する

---
+++ scripts/const.ts
@@ -0,0 +1,17 @@
+export const INDEX_PAGE_LAYOUT = process.env.INDEX_PAGE_LAYOUT || ''
+export const OUTPUT_DIR = process.env.OUTPUT_DIR || ''
+export const PAGE_LAYOUT = process.env.PAGE_LAYOUT || ''
+export const BASE_URL = process.env.BASE_URL || ''
+export const CSS_PATH = process.env.CSS_PATH || ''
+export const INDEX_PAGE_DESCRIPTION = process.env.INDEX_PAGE_DESCRIPTION || ''
+export const INDEX_PAGE_HEADER = process.env.INDEX_PAGE_HEADER || ''
+export const INDEX_PAGE_TITLE = process.env.INDEX_PAGE_TITLE ||''
+export const SOURCE_DIR = process.env.SOURCE_DIR || ''
+export const TITLE = /__TITLE__/g
+export const BODY = '__BODY__'
+export const HEADER = '__HEADER__'
+export const INDEX = '__INDEX__'
+export const CSS = '__CSS__'
+export const URL = '__URL__'
+export const DESCRIPTION = /__DESCRIPTION__/g
+export const SEPARATOR = /---(.*)/s
