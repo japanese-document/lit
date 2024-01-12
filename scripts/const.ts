3d5a1c5c25d363fc14d67dc81349d165afcda776
38elements <mh19820223@gmail.com>
2024-01-12 20:43:14 +0900

    SINGLE_PAGE

--- scripts/const.ts
+++ scripts/const.ts
@@ -8,6 +8,7 @@ export const INDEX_PAGE_HEADER = process.env.INDEX_PAGE_HEADER || ''
 export const INDEX_PAGE_TITLE = process.env.INDEX_PAGE_TITLE ||''
 export const SOURCE_DIR = process.env.SOURCE_DIR || ''
 export const MD_TO_HTML = process.env.MD_TO_HTML === 'true'
+export const SINGLE_PAGE = process.env.SINGLE_PAGE === 'true'
 export const TITLE = /__TITLE__/g
 export const BODY = '__BODY__'
 export const HEADER = '__HEADER__'
